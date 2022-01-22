eeb2adad4a7e7e0397bfc3959832ee68701d0740
devalvez <wesley.alves.jhenp@gmail.com>
2022-01-22 00:19:58 -0300

    Adição de novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const routes = express.Router();
 
+const path = require('path');
+
 const UserController = require("./app/controller/UserController");
 
-routes.get("/", UserController.index);
-routes.post("/", UserController.store);
+routes.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname+'/html/welcome.html'));
+});
+
+routes.get("/request-exemple", function(req, res) {
+    res.sendFile(path.join(__dirname+'/html/request.html'));
+});
+
+routes.get("/user", UserController.index);
+routes.post("/user", UserController.store);
 
 module.exports = routes;
