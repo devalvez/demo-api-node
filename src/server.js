66f16c3b91cab089eb8008308158f1c602eb49d8
devalvez <wesley.alves.jhenp@gmail.com>
2022-01-22 00:47:59 -0300

    Correção no arquivo server.js

--- src/server.js
+++ src/server.js
@@ -3,28 +3,28 @@ const db = require("./database/config");
 const mongoose = require("mongoose");
 
 class App {
-  constructor() {
-    this.express = express();
+    constructor() {
+        this.express = express();
 
-    this.database();
-    this.middlewares();
-    this.routes();
+        this.database();
+        this.middlewares();
+        this.routes();
 
-    this.express.listen(8080, () =>
-      console.log(`Application running on port 8080`);
-    );
-  }
+        this.express.listen(8080, () => {
+            console.log(`Application running on port 8080`);
+        });
+                           }
 
-  database() {
-    mongoose.connect(db.uri, { useNewUrlParser: true });
-  }
+        database() {
+            mongoose.connect(db.uri, { useNewUrlParser: true });
+        }
 
-  middlewares() {
-    this.express.use(express.json());
-  }
+        middlewares() {
+            this.express.use(express.json());
+        }
 
-  routes() {
-    this.express.use(require("./routes"));
-  }
-}
-module.exports = new App().express;
+        routes() {
+            this.express.use(require("./routes"));
+        }
+    }
+    module.exports = new App().express;
